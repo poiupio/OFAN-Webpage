47204f9db65e30ef151da81eb216d5a1e88bda1a
Daniel Rosales <daniel.rosales@lkmx.io>
2021-08-03 19:09:41 -0500

    OFAN V-3.2: Mobile version

--- index.js
+++ index.js
@@ -24,3 +24,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+function showMenuMobile() {
+  document.getElementById("dropdown-menu").classList.toggle("show-menu");
+}
