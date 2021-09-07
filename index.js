371dd5e5e278e5149726bb26e504bee294c01a40
Daniel Rosales <daniel.rosales@lkmx.io>
2021-09-07 17:05:30 -0500

    OFAN V-3.4: added mailer

--- index.js
+++ index.js
@@ -32,3 +32,31 @@ function showMenuMobile() {
 function showLanguageMenuMobile() {
   document.getElementById("dropdown-language").classList.toggle("hidden-dropdrown");
 }
+
+function sendMail() {
+  let mail = document.getElementById("email-input");
+  let phone = document.getElementById("phone-input");
+  let message = document.getElementById("message-input");
+
+  let data = {
+    service_id: 'service_1oja8oo',
+    template_id: 'template_m8t5f9u',
+    user_id: 'user_dWidx44rU3vRX580CreLc',
+    template_params: {
+        'email': mail.value,
+        'message': message.value,
+        'phone_number': phone.value
+    }
+  };
+  
+  $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json'
+  }).done(function() {
+    alert('Mensaje enviado');
+  }).fail(function(error) {
+    console.log(JSON.stringify(error));
+    alert('Intentelo nuevamente');
+  });
+}
