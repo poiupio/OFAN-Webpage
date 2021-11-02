277f5f8896072f212df295d7424de7b832da8059
Daniel Rosales <daniel.rosales@lkmx.io>
2021-11-02 15:01:43 -0600

    OFAN V-4.0

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
 function showMenuMobile() {
   document.getElementById("dropdown-menu").classList.toggle("show-menu");
 }
@@ -56,7 +29,6 @@ function sendMail() {
   }).done(function() {
     alert('Mensaje enviado');
   }).fail(function(error) {
-    console.log(JSON.stringify(error));
     alert('Intentelo nuevamente');
   });
 }
