a70f4b72afa9c7a60225e58b201c4baabf077e28
Daniel Rosales <daniel.rosales@lkmx.io>
2021-11-08 11:24:28 -0600

    OFAN V-4.2: Mobile verssion

--- index.js
+++ index.js
@@ -11,24 +11,28 @@ function sendMail() {
   let phone = document.getElementById("phone-input");
   let message = document.getElementById("message-input");
 
-  let data = {
-    service_id: 'service_1oja8oo',
-    template_id: 'template_m8t5f9u',
-    user_id: 'user_dWidx44rU3vRX580CreLc',
-    template_params: {
-        'email': mail.value,
-        'message': message.value,
-        'phone_number': phone.value
-    }
-  };
-  
-  $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-    type: 'POST',
-    data: JSON.stringify(data),
-    contentType: 'application/json'
-  }).done(function() {
-    alert('Mensaje enviado');
-  }).fail(function(error) {
-    alert('Intentelo nuevamente');
-  });
+  if(mail.value && phone.value && message.value) {
+    let data = {
+      service_id: 'service_1oja8oo',
+      template_id: 'template_m8t5f9u',
+      user_id: 'user_dWidx44rU3vRX580CreLc',
+      template_params: {
+          'email': mail.value,
+          'message': message.value,
+          'phone_number': phone.value
+      }
+    };
+    
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json'
+    }).done(function() {
+      alert('Mensaje enviado');
+    }).fail(function(error) {
+      alert('Intentelo nuevamente');
+    });
+  } else {
+    alert('Datos incompletos')
+  }
 }
